acdbe4fc9cd2b7f54bcf65e4e25b53d0a0965829
unknown <nesad.omeragich27@gmail.com>
2021-04-08 21:15:46 +0200

    Added tests for the solution

---
+++ Diff Two Arrays/challenge.js
@@ -0,0 +1,35 @@
+function hasDuplicate(arr, num) {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length; j++) {
+      if (j !== i && arr[i] === arr[j] && arr[i] === num) {
+        return true;
+      }
+    }
+  }
+  return false;
+}
+
+function diffArray(arr1, arr2) {
+  const con = arr1.concat(arr2);
+  con.sort((a, b) => a - b);
+  const newArr = con.filter((num) => !hasDuplicate(con, num));
+
+  return newArr;
+}
+
+// TESTS
+// diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5]);
+// ["diorite", "andesite", "grass", "dirt", "pink wool", "dead shrub"], ["diorite", "andesite", "grass", "dirt", "dead shrub"] should return ["pink wool"].
+// ["diorite", "andesite", "grass", "dirt", "pink wool", "dead shrub"], ["diorite", "andesite", "grass", "dirt", "dead shrub"] should return an array with one item.
+// ["andesite", "grass", "dirt", "pink wool", "dead shrub"], ["diorite", "andesite", "grass", "dirt", "dead shrub"] should return ["diorite", "pink wool"].
+// ["andesite", "grass", "dirt", "pink wool", "dead shrub"], ["diorite", "andesite", "grass", "dirt", "dead shrub"] should return an array with two items.
+// ["andesite", "grass", "dirt", "dead shrub"], ["andesite", "grass", "dirt", "dead shrub"] should return [].
+// ["andesite", "grass", "dirt", "dead shrub"], ["andesite", "grass", "dirt", "dead shrub"] should return an empty array.
+// [1, 2, 3, 5], [1, 2, 3, 4, 5] should return [4].
+// [1, 2, 3, 5], [1, 2, 3, 4, 5] should return an array with one item.
+// [1, "calf", 3, "piglet"], [1, "calf", 3, 4] should return ["piglet", 4].
+// [1, "calf", 3, "piglet"], [1, "calf", 3, 4] should return an array with two items.
+// [], ["snuffleupagus", "cookie monster", "elmo"] should return ["snuffleupagus", "cookie monster", "elmo"].
+// [], ["snuffleupagus", "cookie monster", "elmo"] should return an array with three items.
+// [1, "calf", 3, "piglet"], [7, "filly"] should return [1, "calf", 3, "piglet", 7, "filly"].
+// [1, "calf", 3, "piglet"], [7, "filly"] should return an array with six items.
