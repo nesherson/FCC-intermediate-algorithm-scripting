2c7eb248f73932460ba41935327440da99d92b54
unknown <nesad.omeragich27@gmail.com>
2021-04-09 23:52:17 +0200

    Added tests for the solution

--- Seek and Destroy/challenge.js
+++ Seek and Destroy/challenge.js
@@ -13,4 +13,10 @@ function destroyer(arr) {
   return newArr;
 }
 
-destroyer([1, 2, 3, 1, 2, 3], 2, 3);
+// TESTS
+// destroyer([1, 2, 3, 1, 2, 3], 2, 3) should return [1, 1].
+// destroyer([1, 2, 3, 5, 1, 2, 3], 2, 3) should return [1, 5, 1].
+// destroyer([3, 5, 1, 2, 2], 2, 3, 5) should return [1].
+// destroyer([2, 3, 2, 3], 2, 3) should return [].
+// destroyer(["tree", "hamburger", 53], "tree", 53) should return ["hamburger"].
+// destroyer(["possum", "trollo", 12, "safari", "hotdog", 92, 65, "grandma", "bugati", "trojan", "yacht"], "yacht", "possum", "trollo", "safari", "hotdog", "grandma", "bugati", "trojan") should return [12,92,65].
