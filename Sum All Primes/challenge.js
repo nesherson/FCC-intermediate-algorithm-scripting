f4ff30b8221dd0e870bf9ecb2e0d11775ae8139a
nesherson <nesad.omeragich27@gmail.com>
2021-04-12 01:12:34 +0200

    Added tests for the solution

--- Sum All Primes/challenge.js
+++ Sum All Primes/challenge.js
@@ -18,4 +18,7 @@ const isPrime = (num) => {
     return primeSum;
   }
   
-  sumPrimes(10);
+// TESTS
+// sumPrimes(10) should return a number.
+// sumPrimes(10) should return 17.
+// sumPrimes(977) should return 73156.
