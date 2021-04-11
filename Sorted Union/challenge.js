b6fbf98a7c142cf476cbee67cabd9764acef0910
nesherson <nesad.omeragich27@gmail.com>
2021-04-11 23:41:20 +0200

    Added tests for the solution

--- Sorted Union/challenge.js
+++ Sorted Union/challenge.js
@@ -6,5 +6,7 @@ function uniteUnique(...arr) {
     return tempArr;
   }
 
-  
-uniteUnique([1, 3, 2], [5, 2, 1, 4], [2, 1]);
+// TESTS
+// uniteUnique([1, 3, 2], [5, 2, 1, 4], [2, 1]);
+// uniteUnique([1, 2, 3], [5, 2, 1]) should return [1, 2, 3, 5].
+// uniteUnique([1, 2, 3], [5, 2, 1, 4], [2, 1], [6, 7, 8]) should return [1, 2, 3, 5, 4, 6, 7, 8].
