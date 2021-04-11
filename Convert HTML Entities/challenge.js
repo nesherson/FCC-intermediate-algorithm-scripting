ee11b82ec90d741bf130fe58abd899e0cb45124b
nesherson <nesad.omeragich27@gmail.com>
2021-04-12 00:07:25 +0200

    Added tests for the solution

--- Convert HTML Entities/challenge.js
+++ Convert HTML Entities/challenge.js
@@ -15,5 +15,12 @@ const operations = [
     }
     return newStr;
   }
-  
-  convertHTML("Dolce & Gabbana");
+
+// TESTS
+// convertHTML("Dolce & Gabbana");
+// convertHTML("Hamburgers < Pizza < Tacos") should return the string Hamburgers &lt; Pizza &lt; Tacos.
+// convertHTML("Sixty > twelve") should return the string Sixty &gt; twelve.
+// convertHTML('Stuff in "quotation marks"') should return the string Stuff in &quot;quotation marks&quot;.
+// convertHTML("Schindler's List") should return the string Schindler&apos;s List.
+// convertHTML("<>") should return the string &lt;&gt;.
+// convertHTML("abc") should return the string abc.
