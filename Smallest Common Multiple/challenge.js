e882e4437fc52446093925586f2bffba22f10f7f
nesherson <nesad.omeragich27@gmail.com>
2021-04-12 01:52:47 +0200

    Added tests for the solution

--- Smallest Common Multiple/challenge.js
+++ Smallest Common Multiple/challenge.js
@@ -20,5 +20,10 @@ const divisibleByAll = (start, end, num) => {
     return counter;
   }
 
-  
-smallestCommons([1, 5]);
+// TESTS
+// smallestCommons([1, 5]) should return a number.
+// smallestCommons([1, 5]) should return 60.
+// smallestCommons([5, 1]) should return 60.
+// smallestCommons([2, 10]) should return 2520.
+// smallestCommons([1, 13]) should return 360360.
+// smallestCommons([23, 18]) should return 6056820.
