168f9ee7756adc001381d85e49c5d7fa89960cad
unknown <nesad.omeragich27@gmail.com>
2021-04-11 21:30:55 +0200

    Added tests for the solution

--- DNA Pairing/challenge.js
+++ DNA Pairing/challenge.js
@@ -14,4 +14,8 @@ function pairElement(str) {
   return pairs;
 }
 
-pairElement('GCG');
+// TESTS
+// pairElement('GCG');
+// pairElement("ATCGA") should return [["A","T"],["T","A"],["C","G"],["G","C"],["A","T"]].
+// pairElement("TTGAG") should return [["T","A"],["T","A"],["G","C"],["A","T"],["G","C"]].
+// pairElement("CTCTA") should return [["C","G"],["T","A"],["C","G"],["T","A"],["A","T"]].
