86d4e770db6ec40017f75f5a6c6274c325946030
nesherson <nesad.omeragich27@gmail.com>
2021-04-11 03:44:25 +0200

    Added tests for the solution

--- Spinal Tap Case/challenge.js
+++ Spinal Tap Case/challenge.js
@@ -6,4 +6,9 @@ const newString = splitByPattern(str);
 return newString.join('-').toLowerCase();
 }
 
-spinalCase('This Is Spinal Tap');
+// TESTS
+// spinalCase('This Is Spinal Tap');
+// spinalCase("thisIsSpinalTap") should return the string this-is-spinal-tap.
+// spinalCase("The_Andy_Griffith_Show") should return the string the-andy-griffith-show.
+// spinalCase("Teletubbies say Eh-oh") should return the string teletubbies-say-eh-oh.
+// spinalCase("AllThe-small Things") should return the string all-the-small-things.
