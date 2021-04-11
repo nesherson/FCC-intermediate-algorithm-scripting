28f9e05101fd6ebec9e03842b605903ec9d30d0e
unknown <nesad.omeragich27@gmail.com>
2021-04-11 21:13:33 +0200

    Added test for the solution

--- Search and Replace/challenge.js
+++ Search and Replace/challenge.js
@@ -16,4 +16,11 @@ function myReplace(str, before, after) {
   return newStr.join(' ');
 }
 
-myReplace('A quick brown fox jumped over the lazy dog', 'jumped', 'leaped');
+// TESTS
+// myReplace('A quick brown fox jumped over the lazy dog', 'jumped', 'leaped');
+// myReplace("Let us go to the store", "store", "mall") should return the string Let us go to the mall.
+// myReplace("He is Sleeping on the couch", "Sleeping", "sitting") should return the string He is Sitting on the couch.
+// myReplace("I think we should look up there", "up", "Down") should return the string I think we should look down there.
+// myReplace("This has a spellngi error", "spellngi", "spelling") should return the string This has a spelling error.
+// myReplace("His name is Tom", "Tom", "john") should return the string His name is John.
+// myReplace("Let us get back to more Coding", "Coding", "algorithms") should return the string Let us get back to more Algorithms.
