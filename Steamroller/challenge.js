d27f979db7637117b0fea23645720276577dca18
nesherson <nesad.omeragich27@gmail.com>
2021-04-12 02:49:07 +0200

    Added tests for the solution

--- Steamroller/challenge.js
+++ Steamroller/challenge.js
@@ -8,5 +8,10 @@ function steamrollArray(arr) {
     }  
     return newArr;
   }
-  
-  steamrollArray([1, [2], [3, [[4]]]]);  
+
+// TESTS
+// steamrollArray([[["a"]], [["b"]]]) should return ["a", "b"].
+// steamrollArray([1, [2], [3, [[4]]]]) should return [1, 2, 3, 4].
+// steamrollArray([1, [], [3, [[4]]]]) should return [1, 3, 4].
+// steamrollArray([1, {}, [3, [[4]]]]) should return [1, {}, 3, 4].
+// Your solution should not use the Array.prototype.flat() or Array.prototype.flatMap() methods.
