c5bb51ba71fc7d0c2e76fdcbc37a2b20e2331714
nesherson <nesad.omeragich27@gmail.com>
2021-04-12 16:10:53 +0200

    Added tests for the solution

--- Map the Debris/challenge.js
+++ Map the Debris/challenge.js
@@ -19,6 +19,6 @@ const getOrbitalPeriod = (radius, gm, avgAlt) => {
     return newArr;
   }
   
-  orbitalPeriod([{name : "sputnik", avgAlt : 35873.5553}]);
-  
-  
+// TESTS
+// orbitalPeriod([{name : "sputnik", avgAlt : 35873.5553}]) should return [{name: "sputnik", orbitalPeriod: 86400}].  
+// orbitalPeriod([{name: "iss", avgAlt: 413.6}, {name: "hubble", avgAlt: 556.7}, {name: "moon", avgAlt: 378632.553}]) should return [{name : "iss", orbitalPeriod: 5557}, {name: "hubble", orbitalPeriod: 5734}, {name: "moon", orbitalPeriod: 2377399}].
