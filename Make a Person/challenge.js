c44710d1e2e17df037e34086db76b894b3fd67db
nesherson <nesad.omeragich27@gmail.com>
2021-04-12 15:13:27 +0200

    Added tests for the solution

--- Make a Person/challenge.js
+++ Make a Person/challenge.js
@@ -31,3 +31,16 @@ var Person = function(firstAndLast) {
   
   var bob = new Person('Bob Ross');
   bob.getFullName();
+// TESTS
+// Object.keys(bob).length should return 6.
+// bob instanceof Person should return true.
+// bob.firstName should return undefined.
+// bob.lastName should return undefined.
+// bob.getFirstName() should return the string Bob.
+// bob.getLastName() should return the string Ross.
+// bob.getFullName() should return the string Bob Ross.
+// bob.getFullName() should return the string Haskell Ross after bob.setFirstName("Haskell").
+// bob.getFullName() should return the string Haskell Curry after bob.setLastName("Curry").
+// bob.getFullName() should return the string Haskell Curry after bob.setFullName("Haskell Curry").
+// bob.getFirstName() should return the string Haskell after bob.setFullName("Haskell Curry").
+// bob.getLastName() should return the string Curry after bob.setFullName("Haskell Curry").
